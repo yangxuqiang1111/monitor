6beb31c8eb6f34d23cb13f59751717cda983e07d
yangxq <yangxq@culiukeji.com>
2016-09-08 15:41:49 +0800

    update

---
+++ monitor-web/src/main/java/com/yangxq/monitor/web/JettyStarter.java
@@ -0,0 +1,94 @@
+package com.yangxq.monitor.web;
+
+import org.apache.jasper.servlet.JspServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.IOException;
+
+/**
+ * Created by Yangxq on 2016/9/7.
+ */
+public class JettyStarter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JettyStarter.class);
+
+    private static final int PORT = 8888;
+
+    private static final String CONTEXT_PATH = "/";
+    private static final String CONFIG_LOCATION_XML = "WEB-INF/mvc-dispatcher-servlet.xml";
+    private static final String MAPPING_URL = "/";
+    private static final String WEBAPP_DIRECTORY = "webapp";
+
+    public static void main(String[] args) throws Exception {
+        new JettyStarter().startJetty(PORT);
+    }
+
+    public void startJetty(int port) throws Exception {
+        LOGGER.debug("Starting server at port {}", port);
+        Server server = new Server(port);
+
+        server.setHandler(getServletContextHandler());
+
+        addRuntimeShutdownHook(server);
+
+        server.start();
+        LOGGER.info("Server started at port {}", port);
+        server.join();
+    }
+
+    private static ServletContextHandler getServletContextHandler() throws IOException {
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS); // SESSIONS requerido para JSP
+        contextHandler.setErrorHandler(null);
+
+        contextHandler.setResourceBase(new ClassPathResource(WEBAPP_DIRECTORY).getURI().toString());
+        contextHandler.setContextPath(CONTEXT_PATH);
+
+        // JSP
+        contextHandler.setClassLoader(Thread.currentThread().getContextClassLoader()); // Necesario para cargar JspServlet
+        contextHandler.addServlet(JspServlet.class, "*.jsp");
+
+        // Spring
+        WebApplicationContext webAppContext = getWebApplicationContext();
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
+        ServletHolder springServletHolder = new ServletHolder("mvc-dispatcher", dispatcherServlet);
+        contextHandler.addServlet(springServletHolder, MAPPING_URL);
+        contextHandler.addEventListener(new ContextLoaderListener(webAppContext));
+
+        return contextHandler;
+    }
+
+    private static WebApplicationContext getWebApplicationContext() {
+        XmlWebApplicationContext context = new XmlWebApplicationContext();
+        context.setConfigLocation(CONFIG_LOCATION_XML);
+
+//        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//        context.setConfigLocation(CONFIG_LOCATION_PACKAGE);
+        return context;
+    }
+
+    private static void addRuntimeShutdownHook(final Server server) {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (server.isStarted()) {
+                    server.setStopAtShutdown(true);
+                    try {
+                        server.stop();
+                    } catch (Exception e) {
+                        System.out.println("Error while stopping jetty server: " + e.getMessage());
+                        LOGGER.error("Error while stopping jetty server: " + e.getMessage(), e);
+                    }
+                }
+            }
+        }));
+    }
+}
