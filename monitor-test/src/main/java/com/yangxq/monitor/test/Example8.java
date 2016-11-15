1c2048b740fd1d35d0fbb0392f8d838a8811dec5
yangxq <yangxq@culiukeji.com>
2016-11-15 10:11:28 +0800

    update

--- monitor-test/src/main/java/com/yangxq/monitor/test/Example8.java
+++ monitor-test/src/main/java/com/yangxq/monitor/test/Example8.java
@@ -1,9 +1,6 @@
 package com.yangxq.monitor.test;
 
-import java.util.EnumMap;
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Yangxq on 2016/11/3.
@@ -15,18 +12,22 @@ public class Example8 {
         enumMap.put(Week.TUESDAY, "星期二");
         enumMap.put(Week.WEDNESDAY, "星期三");
         enumMap.put(Week.WEDNESDAY, "星期四");
-
-        Iterator<Map.Entry<Week, String>> iterator = enumMap.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<Week, String> next = iterator.next();
-            System.out.println("key: " + next.getKey() + ", value： " + next.getValue());
+        String[] str = new String[enumMap.size()];
+        String[] strings = enumMap.values().toArray(str);
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
         }
+//        Iterator<Map.Entry<Week, String>> iterator = enumMap.entrySet().iterator();
+//        while (iterator.hasNext()) {
+//            Map.Entry<Week, String> next = iterator.next();
+//            System.out.println("key: " + next.getKey() + ", value： " + next.getValue());
+//        }
 
         EnumSet<Week> enumSet = EnumSet.allOf(Week.class);
         enumSet.add(Week.MONDAY);
         enumSet.add(Week.TUESDAY);
         Iterator<Week> enumSetIterator = enumSet.iterator();
-        while (enumSetIterator.hasNext()){
+        while (enumSetIterator.hasNext()) {
             Week next = enumSetIterator.next();
             System.out.println(next.name());
 
