ce5a5c9659ffbfc2b45a849b2e229f07ff0299eb
yangxuqiang <18329029859@163.com>
2016-12-09 18:07:59 +0800

    update

--- monitor-test/src/main/java/com/yangxq/monitor/test/Example10.java
+++ monitor-test/src/main/java/com/yangxq/monitor/test/Example10.java
@@ -1,7 +1,5 @@
 package com.yangxq.monitor.test;
 
-import javax.management.openmbean.KeyAlreadyExistsException;
-
 /**
  * Created by Yangxq on 2016/11/22.
  */
@@ -9,4 +7,58 @@ public class Example10 {
     public static void main(String[] args) {
 
     }
+}
+
+interface IBaseCache {
+    /**
+     * master
+     *
+     * @return
+     */
+    String getMaster();
+
+    /**
+     * slaver
+     *
+     * @return
+     */
+    String getSlaver();
+
+    /**
+     * queue
+     *
+     * @return
+     */
+    String getQueue();
+
+    void setMaster(int type);
+}
+
+abstract class BaseCache implements IBaseCache {
+    public String getMaster() {
+         setMaster(1);
+        return "1";
+    }
+
+    public String getSlaver() {
+        return getMaster();
+    }
+
+    public String getQueue() {
+        return getQueue();
+    }
+
+}
+
+class BusinessRedis extends BaseCache implements IBaseMemcache {
+    public void setMaster(int type) {
+        System.out.println("type" + type);
+    }
+
+    public String getMemcache() {
+        return "memcache";
+    }
+}
+interface IBaseMemcache extends IBaseCache{
+    String getMemcache();
 }
